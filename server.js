0ba5e11d264cae835329e6bcb0eba45761eafdfa
Sana <Lenovo@DESKTOP-I4OODFD>
2024-06-25 14:11:31 +0300

    Added new frontend having a sliding sidebar and changed the needed views, added testconnection to server.js and added public files main.css and sidebar.js

--- server.js
+++ server.js
@@ -2,8 +2,12 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-const PORT = 5054
+const { testDatabaseConnection } = require('./TestConnection'); // Update the path
+const PORT = 5055
+
+
 app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')))
 app.use('/transcript',  require(path.join(__dirname, 'routes', 'transcript')))
 app.use('/courseOffering',  require(path.join(__dirname, 'routes', 'courseOffering')))
 app.use('/registration',  require(path.join(__dirname, 'routes', 'registration')))
@@ -11,8 +15,8 @@ app.use('/courseLoad',  require(path.join(__dirname, 'routes', 'courseLoad')))
 app.use('/contractSheet',  require(path.join(__dirname, 'routes', 'contractSheet')))
 app.use('/',  require(path.join(__dirname, 'routes', 'main')))
 
+testDatabaseConnection()
 
-app.use(express.static(path.join(__dirname, 'public')))
 app.listen(PORT, ()=>{
     console.log(`Listening on port ${PORT}`)
 })
